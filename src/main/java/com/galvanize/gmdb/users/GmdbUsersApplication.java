fb56daa6c3edc1c81d2bb09ef8c4e56893a106a7
rlwing <rob@wingjr.com>
2019-07-23 15:59:54 -0700

    Discovry Client

--- src/main/java/com/galvanize/gmdb/users/GmdbUsersApplication.java
+++ src/main/java/com/galvanize/gmdb/users/GmdbUsersApplication.java
@@ -1,9 +1,14 @@
 package com.galvanize.gmdb.users;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GmdbUsersApplication {
 
     public static void main(String[] args) {
