00d898644ce635f730c6c83d3b3487dfa5d6a255
rlwing <rob@wingjr.com>
2019-07-23 16:27:13 -0700

    Convert from maven to gradle

--- src/test/java/com/galvanize/gmdb/users/controllers/UserControllerTests.java
+++ src/test/java/com/galvanize/gmdb/users/controllers/UserControllerTests.java
@@ -2,7 +2,6 @@ package com.galvanize.gmdb.users.controllers;
 
 import com.galvanize.gmdb.users.entities.User;
 import com.galvanize.gmdb.users.services.UserService;
-import com.sun.javafx.binding.StringFormatter;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,19 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import javax.transaction.Transactional;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest @Transactional
