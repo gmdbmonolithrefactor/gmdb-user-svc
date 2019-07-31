5add571c3d818de7a081de13574c8473acc68605
rlwing <rob@wingjr.com>
2019-07-31 09:37:12 -0700

    Getting deployment ready

--- src/main/java/com/galvanize/gmdb/users/services/UserService.java
+++ src/main/java/com/galvanize/gmdb/users/services/UserService.java
@@ -5,6 +5,7 @@ import com.galvanize.gmdb.users.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
